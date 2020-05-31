0a38b14a4afc6652238cee452325ba03d77b8629
Nina Sobiczewska <s19048@pjwstk.edu.pl>
2020-05-31 16:54:12 +0200

    commit

---
+++ cw12/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using cw12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cw12.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
